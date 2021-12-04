16b88729800519a0e32db58b4b7d397a35e437ef
@trinimamud <trinimamud@gmail.com>
2021-12-03 21:03:55 -0300

    cargar pedido

--- src/components/CargarPedido.js
+++ src/components/CargarPedido.js
@@ -1,59 +1,76 @@
+import { useHistory } from "react-router";
 import React, { useState } from 'react';
 import { useHistory } from "react-router-dom";
 import Form from 'react-bootstrap/Form';
 import Button from 'react-bootstrap/Button';
 
+const { React, Component, useState } = require("react");
+const { Form, Button } = require("react-bootstrap");
 
-function CargarPedido() {
+const CargarPedido = () => {
 
+    const history = useHistory();
+    const pedidos = JSON.parse(localStorage.getItem("arrayPedidos")) || [];
 
-    const [pedido, setPedido] = useState({
-        //cosas del pedido
-    })
+    const handleForm = (e) => {
+        const numeroOrden = document.getElementById("id").value;
+        const fechaEntrega = document.getElementById("fecha").value;
+        const cantidad = document.getElementById("cantidad").value;
+        const cliente = document.getElementById("cliente").value;
+        const importe = document.getElementById("importe").value;
 
-    const [pedidos, setPedidos] = useState(['']);
+        const pedido = {
+            numeroOrden,
+            fechaEntrega,
+            cantidad,
+            cliente,
+            importe,
+        };
 
-    let history = useHistory();
+        console.log(pedidos);
+        pedidos.push(pedido);
+        localStorage.arrayPedidos = JSON.stringify(pedidos);
+    };
 
-    let handleForm = (event, valor) => {
-        history.push(
-            {
-                pathname: "/cargarPedido",
-                state: pedidos
-            }
-        );
-        event.stopPropagation();
-        event.preventDefault();
-    }
+    const redireccionar = () => {
+        history.push("/qChurreria");
+    };
 
     return (
         <div className="form">
             <Form onSubmit={handleForm}>
-                <h2>Cargar nuvo pedido</h2>
-                <div className="d-flex justify-content-center">
+                <h2>Cargar nuevo pedido</h2>
+                <div className=" justify-content-center">
                     <div className="p-3 mr-5 ml-5 container">
-                        <Form.Group controlId="nroOrden">
-                            <Form.Control type="number" placeholder="Numero de Orden" name="distance" value={pedido.nroOrden}/>
-                        </Form.Group>
-                        <Form.Group controlId="fechaEntrega">
-                            <Form.Control type="date" placeholder="Fecha de Entrega" name="date" value={pedido.fecha}/>
-                        </Form.Group>
-                        <Form.Group controlId="cantidad">
-                            <Form.Control type="number" placeholder="Cantidad" name="date" value={pedido.cantidad}/>
-                        </Form.Group>
-                        <Form.Group controlId="cliente">
-                            <Form.Control type="text" placeholder="Nombre del Cliente" name="date" value={pedido.cliente}/>
-                        </Form.Group>
-                        <Form.Group controlId="importe">
-                            <Form.Control type="text" placeholder="Importe" name="date" value={pedido.importe}/>
-                        </Form.Group>
+                        <input
+                            disabled
+                            type="text"
+                            id="id"
+                            value={ pedidos.length ? ((pedidos[pedidos.length - 1]).numeroOrden*1) + 1 : 1 }
+                        />
+                    </div>
+                    <div className="p-3 mr-5 ml-5 container">
+                        <input type="date" placeholder="Fecha de Entrega" id="fecha" />
+                    </div>
+                    <div className="p-3 mr-5 ml-5 container">
+                        <input type="number" placeholder="Cantidad" id="cantidad" />
+                    </div>
+                    <div className="p-3 mr-5 ml-5 container">
+                        <input type="text" placeholder="Nombre del Cliente" id="cliente" />
+                    </div>
+                    <div className="p-3 mr-5 ml-5 container">
+                        <input type="text" placeholder="Importe" id="importe" />
                     </div>
                 </div>
-                <Button className="mt-5 mb-5 mr-2 ml-2" size="lg" variant="info" type="submit">Cargar pedido</Button>
+                <Button className="mt-5 mb-5 mr-2 ml-2" size="lg" variant="info" type="submit">
+                    Cargar pedido
+        </Button>
+                <Button className="mt-5 mb-5 mr-2 ml-2" size="lg" variant="info" onClick={redireccionar}>
+                    Cancelar
+        </Button>
             </Form>
         </div>
-    )
-
-}
+    );
+};
 
 export default CargarPedido;
