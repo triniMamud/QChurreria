047a403371139c3b10038d6e5bfd3f56d3c51d5f
@trinimamud <trinimamud@gmail.com>
2021-12-03 21:06:57 -0300

    componente home

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,63 +1,69 @@
-import React, { Component } from 'react';
-import { Button } from 'bootstrap';
-import { Link } from 'react-router-dom';
+import Imagen from "../QChurreria.jpeg";
+const { React, useEffect, useState } = require("react");
+const { Link } = require("react-router-dom");
+const { Table, Container, Button } = require("react-bootstrap");
 
+const Home = () => {
 
-class Home extends Component {
+    const mañana = new Date((new Date).getTime() + (24 * 60 * 60 * 1000));
+    const [pedidos, setPedidos] = useState([]);
 
-    constructor(props) {
-        super(props);
-        this.state = {
-            pedidos: []
-        };
-     }
+    useEffect(() => {
+        setPedidos(JSON.parse(localStorage.getItem('arrayPedidos')) || [])
+    }, []);
 
-    render() {
-        return (
-            <>
-                <div class="firstContainer">
-                    <img src="src\QChurreria.jpeg" alt="QChurreria" />
-                    <div>
-                        <div>
-                            <p>Cantidad de churros a producir mañana: {this.props.pedidos.total}</p>
-                        </div>
-                        <div>
-                            <Button><Link style={{ textDecoration: 'none' }} to='/cargarPedido'><div className="link"></div></Link></Button>
-                        </div>
-                    </div>
-                    <div class="container">
-                        <table>
-                            <tr>
-                                <th>Numero de Orden</th>
-                                <th>Fecha de Entrega</th>
-                                <th>Cantidad</th>
-                                <th>Cliente</th>
-                                <th>Importe</th>
-                            </tr>
-                            {this.props.pedidos.map(pedido => 
-                                <tr>
-                                    <td>{pedido.numeroOrden}</td>
-                                    <td>{pedido.fechaEntrega}</td>
-                                    <td>{pedido.cantidad}</td>
-                                    <td>{pedido.cliente}</td>
-                                    <td>{pedido.importe}</td>
-                                    <td>
-                                        //icono del tachito
-                                    </td>
-                                </tr>
-                            )}
-                            /**
-                            <tr ng-repeat="imagen in imagesList">
-                                
-                            </tr>
-                             */
-                        </table>
-                    </div>
-                </div>
-            </>
-        )
+    const eliminar = (targetId) => {
+        const filtrados = pedidos.filter(pedido => pedido.numeroOrden != targetId);
+        setPedidos(filtrados);
+        localStorage.setItem('arrayPedidos', JSON.stringify(filtrados));
     }
 
+    return (
+        <>
+            <Container>
+                <img src={Imagen} alt="QChurreria" />
+                <div>
+                    <h3>Cantidad de churros a producir mañana: { pedidos
+                        .filter(pedido => 
+                            mañana.getDate() === (new Date(pedido.fechaEntrega+"\n")).getDate() &&
+                            mañana.getMonth() === (new Date(pedido.fechaEntrega+"\n")).getMonth() &&
+                            mañana.getFullYear() === (new Date(pedido.fechaEntrega+"\n")).getFullYear())
+                        .reduce((a, b) => a + (b.cantidad*1 || 0), 0)
+                        }
+                    </h3>
+                </div>
+                <br />
+                <Link to="/cargarPedido" style={{ textDecoration: 'none' }}>
+                    <Button style={{ paddingLeft: 13 }}>Cargar Pedido</Button>
+                </Link>
+                <br /><br />
+                <Table>
+                    <thead><tr>
+                        <th>Número de Órden</th>
+                        <th>Fecha de Entrega</th>
+                        <th>Cantidad</th>
+                        <th>Cliente</th>
+                        <th>Importe</th>
+                        <th></th>
+                    </tr></thead>
+                    <tbody>
+                        {pedidos.map(pedido =>
+                            <tr>
+                                <td>{pedido.numeroOrden}</td>
+                                <td>{pedido.fechaEntrega}</td>
+                                <td>{pedido.cantidad}</td>
+                                <td>{pedido.cliente}</td>
+                                <td>{pedido.importe}</td>
+                                <td>
+                                    <Button color="danger" onClick={()=>eliminar(pedido.numeroOrden)}>Eliminar</Button>
+                                </td>
+                            </tr>
+                        )}
+                    </tbody>
+                </Table>
+            </Container>
+        </>
+    )
 }
 
 export default Home;
